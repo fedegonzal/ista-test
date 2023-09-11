a3726f8eaafc04c3d5e2ffc1a28c0118be12d0fd
Federico Gonzalez Brizzio <fedegonzal@gmail.com>
2023-09-11 12:07:31 +0200

    Sorting Rails version!

--- railsapp/app/controllers/institutions_controller.rb
+++ railsapp/app/controllers/institutions_controller.rb
@@ -1,5 +1,24 @@
 class InstitutionsController < ApplicationController
   def index
-    @institutions = Institution.all    
+    params[:col] ||= "id"
+    params[:dir] ||= "asc"
+    @institutions = Institution.all(params[:col], params[:dir])
+
+
+    # I'm sure that there is a better way to do this :)
+    @sorting = { 
+      id: "",
+      university: "",
+      country: "",
+    }
+    @sorting[params[:col].to_sym] = params[:dir] == "asc" ? "asc" : "desc"
+
+    @action = { 
+      id: "asc",
+      university: "asc",
+      country: "asc",
+    }
+    @action[params[:col].to_sym] = params[:dir] == "asc" ? "desc" : "asc"
+
   end
 end
--- railsapp/app/models/institution.rb
+++ railsapp/app/models/institution.rb
@@ -1,10 +1,11 @@
 class Institution < ApplicationRecord
-    def self.all
+    def self.all(col="id", dir="asc")
         find_by_sql("
         SELECT id, source, university, country, 
         json_extract(location, '$.lat') as lat,
         json_extract(location, '$.lng') as lng
         FROM institutions
+        ORDER BY #{col} #{dir}
         ")
     end    
 end
